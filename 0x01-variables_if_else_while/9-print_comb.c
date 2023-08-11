9cdc9e99b0bde3c13564783e102032cfe57a973f
sham163 <sh.prin1698@gmail.com>
2023-08-11 16:13:40 -0700

    [your message]

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
+
