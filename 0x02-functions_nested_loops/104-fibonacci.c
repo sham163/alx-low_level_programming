5ed5b24eb22e6a34da12364c5e7281e174765ec4
sham163 <sh.prin1698@gmail.com>
2023-08-17 08:29:10 -0700

    [your message]

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -42,7 +42,7 @@ if (f1o > 0)
 printf("%lu", f1o);
 initial0s = numLength(mx) - 1 - numLength(f1);
 
-while (flo > 0 && initial0s > 0)
+while (f1o > 0 && initial0s > 0)
 {
 printf("%d", 0);
 initial0s--;
