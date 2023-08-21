d1c2826e746550f0163729644a25fad727a3a402
sham163 <sh.prin1698@gmail.com>
2023-08-21 06:55:52 -0700

    [your message]

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,22 +1,24 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * Description: a function that returns the length of a string.
+ * Description: _strlen - a function that returns the length of a string.
  *
  * @s: string parameter
+ *
+ * Return: length the length of string
  */
 
 int _strlen(char *s)
 {
-       int i = 0;
-       int length = 0;
-       
-       char sham = *s;
-       
-       while (s[i] != '\0'){
-           length += 1;
-           i++;
-       }
-       return (length);
+int i = 0;
+int length = 0;
+
+char sham = *s;
+
+while (s[i] != '\0')
+{
+length += 1;
+i++;
+}
+return (length);
 }
