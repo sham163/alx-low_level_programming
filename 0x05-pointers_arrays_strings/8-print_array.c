f5b6f76aa81f5ff98cd23c4b382810a85dce78f7
sham163 <sh.prin1698@gmail.com>
2023-08-21 17:39:31 -0700

    [your message]

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n - 1; i++)
 	{
-		printf(a[i]);
+		printf("%d", a[i]);
 		_putchar(44);
 	}
 	_putchar('\n');
