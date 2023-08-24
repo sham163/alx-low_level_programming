f1936d2515bf00ed4b147f45933d93024348c761
sham163 <sh.prin1698@gmail.com>
2023-08-23 17:02:23 -0700

    [your message]

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,16 +13,11 @@
 int _strcmp(char *s1, char *s2)
 {
 int i;
-
 i = 0;
-
-
 if (s1[i] < s2[i])
 return (s1[i] - s2[i]);
-
 else if (s1[i] > s2[i])
 return (s1[i] - s2[i]);
-
 else
 return (s1[i] - s2[i]);
 }
