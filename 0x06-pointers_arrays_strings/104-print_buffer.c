07a6218fb9ee8130df2ea3869c061bbc5aa7ddb6
sham163 <sh.prin1698@gmail.com>
2023-08-24 19:56:11 -0700

    [your message]

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Description: print_buffer -  a function that prints a buffer.
+ *
+ * @b: char input parameter
+ * @size: int input parametr
+ */
+
+
+void print_buffer(char *b, int size)
+{
+
