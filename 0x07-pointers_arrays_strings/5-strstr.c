16b16fca4944c45e3736538c5b22bed8aafa6aef
sham163 <sh.prin1698@gmail.com>
2023-08-28 19:03:45 -0700

    [your message]

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,4 +2,34 @@
 
 /**
  * Description: _strstr -  a function that locates a substring.
- * @haystack: 
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: pointer to needle
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+char *k;
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i] == needle[j])
+{
+k = &haystack[i];
+return (k);
+}
+else
+
+
+if (haystack[i] != needle[j])
+break;
+}
+
+
+}
+return (0);
+}
