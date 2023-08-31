307930f173c43c6dfeba91e133e7a9271e91904d
sham163 <sh.prin1698@gmail.com>
2023-08-30 19:11:26 -0700

    [your message]

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * Description: is_palindrome - a function that returns 1
+ * if a string is a palindrome and 0 if not.
+ * @s: string
+ * Return: returns 1 if a string is a palindrome and 0 if not.
+ */
