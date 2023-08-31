000898533f5932bddaf9db410d184caf349914d2
sham163 <sh.prin1698@gmail.com>
2023-08-30 17:43:16 -0700

    [your message]

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,23 +4,30 @@
  * Description: is_prime_number - a function that returns 1
  * if the input integer is a prime number, otherwise return 0.
  * @n: input number
+ * @j: int
  * Return: returns 1 if the input integer is a prime number,
  * otherwise return 0.
  */
 
+int check_prime(int n, int j);
 int is_prime_number(int n)
 {
-if (n <= 1)
-{
-return (0);
-}
-for (int i = 2; i * i <= n; ++i)
-{
-if (n % i == 0)
-{
-return (0);
-}
+return (check_prime(n, 2));
 }
 
+/**
+ * Description: check_prime - check all number < n
+ * @n: int
+ * @j: int
+ * Return: int
+ */
+
+int check_prime(int n, int j)
+{
+if (j >= n && n > 1)
 return (1);
+else if (n % j == 0 || n <= 1)
+return (0);
+else
+return (check_prime(n, j + 1));
 }
