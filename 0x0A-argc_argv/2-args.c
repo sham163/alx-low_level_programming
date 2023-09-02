8b7b4a7798d838aa18183ecd0f6e0df1467d01fc
sham163 <sh.prin1698@gmail.com>
2023-09-01 19:05:10 -0700

    [your message]

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Description: main - a program that prints all arguments it receives.
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%s\n", argv[i]);
+		return (0);
+	}
+}
+
