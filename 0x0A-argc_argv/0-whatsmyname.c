b5c95cd2b4adc867e8ba63beb5b332311e84c55a
sham163 <sh.prin1698@gmail.com>
2023-09-01 18:39:37 -0700

    [your message]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Description: program that prints its name, followed by a new line.
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
