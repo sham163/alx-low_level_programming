bad20a6f94808ae1c84e42c31b51ea9b191cac93
sham163 <sh.prin1698@gmail.com>
2023-09-01 18:52:58 -0700

    [your message]

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Description: main - program that prints the number of arguments passed into it.
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+
+int main(int argc, char const **argv)
+{
+	(void)*argv;
+	printf("%d\n", argc);
+	return (0);
+}
