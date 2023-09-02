67606fdeb63ea1ed97ed9c4aa0c55086c7cf65e6
sham163 <sh.prin1698@gmail.com>
2023-09-01 19:45:32 -0700

    [your message]

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,17 @@
 
 int main(int argc, char *argv[])
 {
-	int mu = 1;
+	int mu;
 
 	if (argc == 3)
 	{
-		mu = mu *  atoi(argv[1] * argv[2]);
+		mu = atoi(argv[1] * argv[2]);
 		printf("%d\n", mu);
-		return (0);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
