414114483182478b79d824fab2eb61dfc7e8058c
sham163 <sh.prin1698@gmail.com>
2023-09-05 17:18:47 -0700

    [your message]

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * Description: alloc_grid - a function that returns
+ * a pointer to a 2 dimensional array of integers.
+ * @width: width of array
+ * @height: height of array
+ * Return: return NULL on failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **sham, s, t;
+
+
+	if (width <= 0)
+		return (NULL);
+	if (height <= 0)
+		return (NULL);
+
+	sham = malloc(sizeof(*sham) * height);
+
+	if (sham == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (s = 0; s < height; s++)
+		{
+			sham[s] = malloc(sizeof(**sham) * width);
+			
+			if (sham[s] == NULL)
+			{
+				free(sham);
+				return(NULL);
+			}
+
+			for (j = 0; j < width; j++)
+			{
+				sham[s][t] = 0;
+			}
+		}
+	}
+	return (sham);
+}
+
+
