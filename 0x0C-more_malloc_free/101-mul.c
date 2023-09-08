c98d74fa7fbe90716f6ec7371656a0aeedb40a2b
sham163 <sh.prin1698@gmail.com>
2023-09-07 19:44:11 -0700

    [your message]

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Description: print_int - print an int
+ * @n: int
+ * Return: 0
+ */
+
+void print_int(unsigned long int n)
+{
+	unsigned long int d = 1, i, r;
+
+	for (i = 0; n / d > 9; i++, d *= 10)
+		;
+
+	for (; d >= 1; n %= d, d /= 10)
+	{
+		r = n / d;
+		_putchar('0' + r);
+	}
+}
+
+
+
+/**
+ * Description: main -  multiplies two positive numbers.
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	print_int(atoi(argv[1]) * atoi(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
+
+
