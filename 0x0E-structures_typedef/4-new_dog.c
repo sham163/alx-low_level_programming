486e9d94006e67e2722d256abb6cff21f46a462c
sham163 <sh.prin1698@gmail.com>
2023-09-11 19:40:00 -0700

    [your message]

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,67 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * Description: _strcopy - copy the string
+ * @src: string
+ * @tes: copy string
+ * Return: tes
+ */
+
+char *_strcopy(char *tes, char *src)
+{
+	int s;
+
+	for (s = 0; src[s]; s++)
+		tes[s] = src[s];
+	tes[s] = '\0';
+
+	return (tes);
+}
+
+/**
+ * Description: new_dog - a function that create a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!owner || !name || age < 0)
+		return (NULL);
+
+	dog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = _strcopy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcopy(dog->owner,owner);
+
+	return (dog);
+
+}
