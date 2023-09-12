0202143bef6c79a79dae443864f1f3e57de6eb27
sham163 <sh.prin1698@gmail.com>
2023-09-11 18:50:10 -0700

    [your message]

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * Description: struct dog - a dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
