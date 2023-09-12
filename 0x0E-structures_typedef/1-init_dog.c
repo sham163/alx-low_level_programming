8cafa03a9f0c7cc3aae00dfad3d01018b173bc59
sham163 <sh.prin1698@gmail.com>
2023-09-11 18:55:42 -0700

    [your message]

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * Description: struct dog - a dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
