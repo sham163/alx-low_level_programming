9b9e7b258d6da9f58739d9ab8219b74ed8e389b9
sham163 <sh.prin1698@gmail.com>
2023-09-13 18:46:55 -0700

    [your message]

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	f = array + size - 1;
 
-	if (array != NULL && size != NULL && action != NULL)
+	if (array != NULL && size != 0 && action != NULL)
 	{
 		while (f >= array)
 		{
