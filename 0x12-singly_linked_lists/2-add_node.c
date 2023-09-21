2eef4e31500a61157cd81f4737e1383dcce79ddb
sham163 <sh.prin1698@gmail.com>
2023-09-21 16:09:38 -0700

    h

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* Description: add_node - add_node
+* @head: head
+* @str: str
+* Return: the address of the new element, or NULL if it failed
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *n = malloc(sizeof(list_t));
+
+if (n == NULL || str == NULL)
+return (NULL);
+if (str != NULL)
+{
+n->str = strdup(str);
+
+if (!n->str)
+{
+free(n);
+return (NULL);
+}
+n->len = _strlen(n->str);
+}
+n->next = *head;
+*head = n;
+return (n);
+
