73b8baa6629f2995a3a072b76330d2ec6a7d3d2e
sham163 <sh.prin1698@gmail.com>
2023-09-21 15:10:03 -0700

    h

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* Dwscription: print_list - a function that prints all the elements
+* @h: pointer to head
+* Return: the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t c;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+
+c++;
+h = h->next;
+}
+return (c);
+}
