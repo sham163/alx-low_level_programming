c25c6f7d27022b2a65055ed0b55f1a61d214accf
sham163 <sh.prin1698@gmail.com>
2023-09-21 17:51:41 -0700

    h

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+void before(void) __attribute__((constructor));
+
+void before(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
