fba63a07d03d2e9245743a32ed5790862e6c2ae2
sham163 <sh.prin1698@gmail.com>
2023-09-26 17:09:24 -0700

    l

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * Description: print_listint - a function that prints all the elements
+ * @h: h
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t s = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		s++;
+	}
+	return (s);
+}
