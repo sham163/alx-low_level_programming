72688a70b5e54eaeedd8e4c70a35a139b933bfc6
sham163 <sh.prin1698@gmail.com>
2023-09-26 18:17:21 -0700

    l

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	nn = malloc(sizeof(listint_t));
 
-	if (nn == NULL || head == NUlL)
+	if (nn == NULL || head == NULL)
 	{
 		return (NULL);
 	}
