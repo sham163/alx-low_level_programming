76437fcb920b468dea9725cedab30ba785bc63a3
sham163 <sh.prin1698@gmail.com>
2023-09-27 16:24:11 -0700

    k

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * Description: print_listint_safe - a function that prints a listint_t
+ * @head: head
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+
