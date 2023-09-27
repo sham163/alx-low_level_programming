d3c1efb9fb2b843365655da25c40796a730df17c
sham163 <sh.prin1698@gmail.com>
2023-09-26 18:26:22 -0700

    l

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,7 +5,7 @@
 * deletes the node at index
 * @head: head
 * @index: index
-* Returns: 1 if it succeeded, -1 if it failed
+* Return: 1 if it succeeded, -1 if it failed
 */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
