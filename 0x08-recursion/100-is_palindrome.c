dcf165eaefa52db5816e8a430f9067f74dd0c4fd
sham163 <sh.prin1698@gmail.com>
2023-09-27 17:11:38 -0700

    k

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,8 +1,53 @@
 #include "main.h"
 
 /**
+ * Description: l_index - return the last index
+ * @s: s
+ * Return: int
+ */
+
+int l_index(char *s)
+{
+	int m = 0;
+
+	if (*s > '\0')
+		m = m + l_index(s + 1) + 1;
+
+	return (m);
+}
+
+
+/**
  * Description: is_palindrome - a function that returns 1
  * if a string is a palindrome and 0 if not.
  * @s: string
  * Return: returns 1 if a string is a palindrome and 0 if not.
  */
+
+int is_palindrome(char *s)
+{
+	int e;
+
+	e = l_index(s);
+
+	return (check(s, 0, e - 1, e % 2));
+}
+
+/**
+ * Description: ch - checks for the palindrome string
+ * @s: s
+ * @st: st
+ * @e: e
+ * @mo: mo
+ * Return: 0 or 1
+ */
+
+int ch(char *s, int st, int e, int mo)
+{
+	if ((st == e + 1 && mo == 0) || (st == e && mo != 0))
+		return (1);
+	else if (s[st] != s[e])
+		return (0);
+	else
+		return (ch(s, st + 1, e - 1, mo));
+}
