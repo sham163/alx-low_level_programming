9905eb9a8e90156dc0a435860a7610c5e3bab8dd
sham163 <sh.prin1698@gmail.com>
2023-09-27 19:29:34 -0700

    k

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(char *start, int num_bytes) {
-    for (int i = 0; i < num_bytes; i++) {
+void print_opcodes(char *start, int num_bytes)
+{
+	int i;
+    for (i = 0; i < num_bytes; i++)
+    {
         printf("%02hhx", start[i]);
-        if (i < num_bytes - 1) {
+        if (i < num_bytes - 1)
+	{
             printf(" ");
-        } else {
+        }
+       	else
+	{
             printf("\n");
         }
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         fprintf(stderr, "Error\n");
         return 1;
     }
 
     int num_bytes = atoi(argv[1]);
 
-    if (num_bytes < 0) {
+    if (num_bytes < 0)
+    {
         fprintf(stderr, "Error\n");
         return 2;
     }
 
     char *main_address = (char *)main;
 
-    // Print the opcodes
+    
     print_opcodes(main_address, num_bytes);
 
     return 0;
