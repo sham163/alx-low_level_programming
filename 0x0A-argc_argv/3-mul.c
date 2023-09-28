fed3d16229233d5cf7c9f8664453e9efc61215fe
sham163 <sh.prin1698@gmail.com>
2023-09-27 18:05:03 -0700

    h

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,25 +2,23 @@
 #include <stdio.h>
 
 /**
- * Description: main - a program that multiplies two numbers.
- * @argc: count
- * @argv: value
- * Return: 0 if success
- */
+* Description: main - a program that multiplies two numbers.
+* @argc: count
+* @argv: value
+* Return: 0 if success
+*/
 
 int main(int argc, char *argv[])
 {
-	int mu;
+if (argc != 3) {
+printf("Error\n");
+return (1);
+}
 
-	if (argc == 3)
-	{
-		mu = atoi(argv[1] * argv[2]);
-		printf("%d\n", mu);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
-	return (0);
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+int result = num1 * num2;
+
+printf("%d\n", result);
+return (0);
 }
