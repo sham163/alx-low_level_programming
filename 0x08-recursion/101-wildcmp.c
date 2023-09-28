b7cd2c934a36264e047f7bf36c0621f7f2c1b9aa
sham163 <sh.prin1698@gmail.com>
2023-09-27 17:39:55 -0700

    k

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,6 +15,32 @@ char *mps(char *s2)
 }
 
 
+
+/**
+ * Description: inc - inc function
+ * @s1: s1
+ * @s2: s2
+ * Return: int
+ */
+
+int inc(char *s1, char *s2)
+{
+        int r = 0;
+
+        if (*s1 == 0)
+                return (0);
+
+        if (*s1 == *s2)
+        {
+                r = r + wildcmp(s1 + 1, s2 + 1);
+        }
+        r = r + inc(s1 + 1, s2);
+        return (r);
+}
+
+
+
+
 /**
  * Description: wildcmp - wildcmp function
  * @s1: s1
@@ -26,7 +52,7 @@ int wildcmp(char *s1, char *s2)
 {
 	int r = 0;
 
-	if (*s2 && !*s1 == '*' && !mps(s2))
+	if (!*s1 && *s2 == '*' && !mps(s2))
 	{
 		return (1);
 	}
@@ -60,24 +86,3 @@ int wildcmp(char *s1, char *s2)
 
 
 
-/**
- * Description: inc - inc function
- * @s1: s1
- * @s2: s2
- * Return: int
- */
-
-int inc(char *s1, char *s2)
-{
-	int r = 0;
-
-	if (*s1 == 0)
-		return (0);
-
-	if (*s1 == *s2)
-	{
-		r = r + wildcmp(s1 + 1, s2 + 1);
-	}
-	r = r + inc(s1 + 1, s2);
-	return (r);
-}
