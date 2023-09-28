e934c81dda1e87f8aa77d96c479a17b1ca9f0498
sham163 <sh.prin1698@gmail.com>
2023-09-27 18:07:41 -0700

    h

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,17 @@
 
 int main(int argc, char *argv[])
 {
+int n1, n2, r;
+
 if (argc != 3) {
 printf("Error\n");
 return (1);
 }
 
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
-int result = num1 * num2;
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+r = n1 * n2;
 
-printf("%d\n", result);
+printf("%d\n", r);
 return (0);
 }
