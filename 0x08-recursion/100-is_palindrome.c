1191fae53e8b29d2ebced3aa21953fb268ddeb0f
sham163 <sh.prin1698@gmail.com>
2023-09-27 17:16:16 -0700

    k

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -18,6 +18,25 @@ int l_index(char *s)
 
 
 /**
+ * Description: check - checks for the palindrome string
+ * @s: s
+ * @st: st
+ * @e: e
+ * @mo: mo
+ * Return: 0 or 1
+ */
+
+int check(char *s, int st, int e, int mo)
+{
+	if ((st == e + 1 && mo == 0) || (st == e && mo != 0))
+		return (1);
+	else if (s[st] != s[e])
+		return (0);
+	else
+		return (check(s, st + 1, e - 1, mo));
+}
+
+/**
  * Description: is_palindrome - a function that returns 1
  * if a string is a palindrome and 0 if not.
  * @s: string
@@ -26,28 +45,9 @@ int l_index(char *s)
 
 int is_palindrome(char *s)
 {
-	int e;
+        int e;
 
-	e = l_index(s);
+        e = l_index(s);
 
-	return (check(s, 0, e - 1, e % 2));
-}
-
-/**
- * Description: ch - checks for the palindrome string
- * @s: s
- * @st: st
- * @e: e
- * @mo: mo
- * Return: 0 or 1
- */
-
-int ch(char *s, int st, int e, int mo)
-{
-	if ((st == e + 1 && mo == 0) || (st == e && mo != 0))
-		return (1);
-	else if (s[st] != s[e])
-		return (0);
-	else
-		return (ch(s, st + 1, e - 1, mo));
+        return (check(s, 0, e - 1, e % 2));
 }
