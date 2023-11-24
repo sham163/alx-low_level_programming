9c5e3df13905dd21e80071980c34ce616ff9f980
sham163 <sh.prin1698@gmail.com>
2023-11-23 16:08:01 -0800

    hi

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * Description: int flip_bits - unction that returns the number of bits
+ * @n: number
+ * @m: m
+ * Return: number
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x = n ^ m;
+	unsigned int c = 0;
+
+	while (x)
+	{
+		if (x & 1ul)
+			c++;
+		x = x >> 1;
+	}
+	return (c);
+}
