2958fa868d716deaa3fcfcd022253312daaad281
sham163 <sh.prin1698@gmail.com>
2023-11-27 06:00:24 -0800

    hi

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,54 +1,54 @@
 #include "main.h"
 
 /**
- * Description: _strlen - length of string
- * @s: string
- * Return: integer
- */
+* Description: _strlen - length of string
+* @s: string
+* Return: integer
+*/
 
 int _strlen(char *s)
 {
-        int j = 0;
+int j = 0;
 
-        if (s == NULL)
-        {
-                return (0);
-        }
+if (s == NULL)
+{
+return (0);
+}
 
-        while (*s++)
-        {
-                j++;
-        }
+while (*s++)
+{
+j++;
+}
 
-        return (j);
+return (j);
 }
 
 /**
- * Description: append_text_to_file - unction that appends text
- * at the end of a file
- * @filename: filename
- * @text_content: text_content
- * Return: 1 on success and -1 on failure
- */
+* Description: append_text_to_file - unction that appends text
+* at the end of a file
+* @filename: filename
+* @text_content: text_content
+* Return: 1 on success and -1 on failure
+*/
 
 int append_text_to_file(const char *filename, char *text_content)
 {
 int f;
-        ssize_t b = 0, l = _strlen(text_content);
+ssize_t b = 0, l = _strlen(text_content);
 
-        if (!filename)
-        {
-                return (-1);
-        }
+if (!filename)
+{
+return (-1);
+}
 
-        f = open(filename, O_WRONLY | O_APPEND);
+f = open(filename, O_WRONLY | O_APPEND);
 
-        if (f == -1)
-                return (-1);
+if (f == -1)
+return (-1);
 
-        if (l)
-                b = write(f, text_content, l);
+if (l)
+b = write(f, text_content, l);
 
-        close(f);
-        return (b == l ? 1 : -1);
+close(f);
+return (b == l ? 1 : -1);
 }
